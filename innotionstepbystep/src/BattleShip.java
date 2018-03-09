7452d04033c85b279588229e284124eee423f497
Tomek <tomasz.ciecwierz@gmail.com>
2018-03-09 09:09:35 +0100

    czyszczenie

--- innotionstepbystep/src/BattleShip.java
+++
@@ -1,9 +0,0 @@
-public class BattleShip {
-    public Cord leftTop;
-    public Cord rightBottom;
-
-    BattleShip(Cord first, Cord second) {
-        this.leftTop = first;
-        this.rightBottom = second;
-    }
-}
